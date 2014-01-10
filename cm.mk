26b955bf37d7fb1d1242bf2a8d39fde890005a9b
Scott <Tortel1210@gmail.com>
2014-01-10 10:34:04 -0500

    Update fingerprint

--- cm.mk
+++ cm.mk
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=jgedlteue \
     TARGET_DEVICE=jgedlte \
-    BUILD_FINGERPRINT="samsung/jgedlteue/jgedlte:4.3/JWR66Y.S003/130805:user/release-keys" \
-    PRIVATE_BUILD_DESC="jgedlteue-user 4.3 JWR66Y.S003 130805 release-keys"
+    BUILD_FINGERPRINT="samsung/jgedlteue/jgedlte:4.4.2/KOT49H.S001/131204:user/release-keys" \
+    PRIVATE_BUILD_DESC="jgedlteue-user 4.4.2 KOT49H.S001 131204 release-keys"
 
 PRODUCT_DEVICE := jgedlte
 PRODUCT_NAME := cm_jgedlte
